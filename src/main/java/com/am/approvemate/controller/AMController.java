2a63d8277a4d08eefe797cce2f3ece5de5673434
Vaibhav Gupta <vaibhav.h.gupta@oracle.com>
2025-05-28 12:40:27 +0530

    added logic to fetch licensingRecord for BA

--- src/main/java/com/am/approvemate/controller/AMController.java
+++ src/main/java/com/am/approvemate/controller/AMController.java
@@ -1,6 +1,7 @@
 package com.am.approvemate.controller;
 
 import com.am.approvemate.model.Library;
+import com.am.approvemate.model.LicensingRecord;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,24 @@ public class AMController {
     @GetMapping("/getDetails")
     public ResponseEntity<List<Library>> sendDetails(){
         try {
-            InputStream inputStream = new ClassPathResource("static/java_libraries_with_versions.json").getInputStream();
+            InputStream inputStream = new ClassPathResource("static/updated_java_libraries.json").getInputStream();
             ObjectMapper mapper = new ObjectMapper();
             List<Library> libraries = mapper.readValue(inputStream, new TypeReference<List<Library>>() {});
             return ResponseEntity.ok(libraries);
         } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/getLicensingRecord")
+    public ResponseEntity<List<LicensingRecord>> sendLicensingRecord(){
+        try {
+            InputStream inputStream = new ClassPathResource("static/licensing_data_synced_with_libraries.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            List<LicensingRecord> licensingRecords = mapper.readValue(inputStream, new TypeReference<List<LicensingRecord>>() {});
+            return ResponseEntity.ok(licensingRecords);
+        } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.internalServerError().build();
         }
     }
